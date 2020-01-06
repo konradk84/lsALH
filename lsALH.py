29f831cd5a3abe66e7683a32074339e1c211aa5b
konradk84 <konrad.karasinski84@gmail.com>
2020-01-06 10:09:29 +0100

    print fix

--- lsALH.py
+++ lsALH.py
@@ -16,7 +16,7 @@ def get_size_format(b, factor=1024, suffix="B"):
     """
     for unit in ["", "K", "M", "G", "T", "P", "E", "Z"]:
         if b < factor:
-            return f"{b:.2f}{unit} {suffix}"
+            return f"{b:.2f} {unit}{suffix}"
         b /= factor
     return f"{b:.2f}Y {suffix}"
 
